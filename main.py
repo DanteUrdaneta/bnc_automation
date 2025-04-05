6459a969ebf67b3f675ac081906bc9524e2619e0
DanteUrdaneta <danteurd@gmail.com>
2025-04-05 00:02:25 -0400

    5 minutes

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
 import pandas
 import time
 
@@ -12,115 +11,79 @@ import time
 coordenada_ruta = 'cordenadas.csv'
 credenciales_ruta = 'credenciales.csv'
 
-
 df = pandas.read_csv(coordenada_ruta, dtype=str)
-credenciales = pandas.read_csv(credenciales_ruta, dtype=str)
+credenciales = pandas.read_csv(credenciales_ruta, dtype=str).iloc[0]  # Tomamos solo una fila
 
-options = webdriver.ChromeOptions()
-options.add_experimental_option("detach", True)
+def crear_driver():
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("detach", False)
+    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+def login(driver, credenciales):
+    driver.get("https://personas.bncenlinea.com/Auth/Login")
+    time.sleep(5)
+    login_buttons = ['CardNumber', 'UserID', 'BtnSend', 'UserPassword']
 
+    driver.find_element(By.NAME, login_buttons[0]).send_keys(credenciales['cuenta'])
+    driver.find_element(By.ID, login_buttons[1]).send_keys(credenciales['cedula'])
+    driver.find_element(By.ID, login_buttons[2]).click()
 
-driver.get("https://personas.bncenlinea.com/Auth/Login")
+    time.sleep(1)
+    driver.find_element(By.NAME, login_buttons[3]).send_keys(credenciales['password'])
+    driver.find_element(By.ID, login_buttons[2]).click()
 
-time.sleep(5)
+def debitar_bs_cuenta(driver):
+    driver.find_element(By.XPATH, "//button[@data-id='idAccount_VES']").click()
+    time.sleep(1)
+    driver.find_element(By.ID, 'bs-select-1-1').click()
 
+def us_cuenta(driver):
+    driver.find_element(By.XPATH, "//button[@data-id='idAccount_Currency']").click()
+    time.sleep(1)
+    driver.find_element(By.ID, 'bs-select-2-1').click()
 
-def  login(credenciales):
-  login_buttons = ['CardNumber', 'UserID', 'BtnSend', 'UserPassword']
+def monto(driver, usd):
+    driver.find_element(By.ID, 'Amount_Currency').send_keys(usd)
+    driver.find_element(By.XPATH, """//*[@id="Frm_Purchase_DoOperation"]/div/div[9]/div/div[1]/button""").click()
 
-  cardnumber = driver.find_element(By.NAME, login_buttons[0])
-  cardnumber.send_keys(credenciales['cuenta'])
-
-  userid = driver.find_element(By.ID, login_buttons[1])
-  userid.send_keys(credenciales['cedula'])
-
-  button = driver.find_element(By.ID, login_buttons[2])
-  button.click()
-
-  time.sleep(1)
-
-  password = driver.find_element(By.NAME, login_buttons[3])
-  password.send_keys(credenciales['password'])
-
-  button = driver.find_element(By.ID, login_buttons[2])
-  button.click()
-  
-def debitar_bs_cuenta():
-  cuenta = driver.find_element(By.XPATH, "//button[@data-id='idAccount_VES']")
-  cuenta.click()
-  time.sleep(1)
-  option = driver.find_element(By.ID, 'bs-select-1-1')
-  option.click()
-
-  
-def us_cuenta():
-  cuenta = driver.find_element(By.XPATH, "//button[@data-id='idAccount_Currency']")
-  cuenta.click()
-  time.sleep(1)
-  option = driver.find_element(By.ID, 'bs-select-2-1')
-  option.click()
-  
-  
-def monto(usd):
-  input = driver.find_element(By.ID, 'Amount_Currency')
-  input.send_keys(usd)
-  element = driver.find_element(By.XPATH, """//*[@id="Frm_Purchase_DoOperation"]/div/div[9]/div/div[1]/button""")
-  element.click()
-
-def confirmar():
-    element = driver.find_element(By.XPATH, """//*[@id="Frm_Purchase_Verify"]/div/div[9]/div/div[1]/button""")
-    element.click()
+def confirmar(driver):
+    driver.find_element(By.XPATH, """//*[@id="Frm_Purchase_Verify"]/div/div[9]/div/div[1]/button""").click()
     time.sleep(3)
     try:
-      control = WebDriverWait(driver, 10).until(
-      EC.presence_of_element_located((By.ID, "ControlNumber"))
-  )
-      control.send_keys('29305558') # PONER NUMERO DE CONTROL DE LAS CORDENADAS
-      cordenada = WebDriverWait(driver, 10).until(
-      EC.presence_of_element_located((By.XPATH, """//*[@id="Frm_AuthFactor_Set"]/div/div/div[2]/div/div/div/div/div/span""")))    
-      cor = cordenada.text #example D-9
-      # separar la cordenada
-      cordenadas = cor.split('-')
-      cor = df.loc[int(cordenadas[1])-1, cordenadas[0]]
-      print(f"poniendo cordenadas {cor}")
-      input = WebDriverWait(driver, 10).until(
-      EC.presence_of_element_located((By.ID, "Value"))
-      )
-      input.send_keys(cor)
-    
-      submit = driver.find_element(By.XPATH, """//*[@id="Mdl-Content2"]/div/div/div[2]/div[2]/button[1]""")
-      submit.click()
-      time.sleep(1)
-      print('compra exitosa')
+        control = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "ControlNumber")))
+        control.send_keys('29305558')  # PONER NUMERO DE CONTROL
+
+        cordenada = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, """//*[@id="Frm_AuthFactor_Set"]/div/div/div[2]/div/div/div/div/div/span""")))
+        cor = cordenada.text.split('-')
+        valor = df.loc[int(cor[1]) - 1, cor[0]]
+        print(f"Poniendo coordenadas {valor}")
+        
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "Value"))).send_keys(valor)
+        driver.find_element(By.XPATH, """//*[@id="Mdl-Content2"]/div/div/div[2]/div[2]/button[1]""").click()
+        print('Compra exitosa')
+        time.sleep(1)
     except:
-      submit = driver.find_element(By.XPATH, """//*[@id="Mdl-Content2"]/div/div/div[2]/div[2]/button[1]""")
-      submit.click()
-      print('compra exitosa')
-      time.sleep(1)
-    
-  
-login(credenciales)
+        driver.find_element(By.XPATH, """//*[@id="Mdl-Content2"]/div/div/div[2]/div[2]/button[1]""").click()
+        print('Compra exitosa (sin coordenada)')
+        time.sleep(1)
 
-time.sleep(1)
-
-
-
-
+# Bucle principal
 while True:
-  
-  driver.get('https://personas.bncenlinea.com/ForeignExchange/ForeignExchange/Purchase')
+    try:
+        driver = crear_driver()
+        login(driver, credenciales)
+        time.sleep(2)
+        driver.get('https://personas.bncenlinea.com/ForeignExchange/ForeignExchange/Purchase')
+        time.sleep(2)
 
-  try:
-    time.sleep(1)
-    debitar_bs_cuenta()
-    us_cuenta()
+        debitar_bs_cuenta(driver)
+        us_cuenta(driver)
+        monto(driver, "20")  # Puedes cambiar el monto aquí
+        confirmar(driver)
 
-    monto(20)
-    time.sleep(1)
-    confirmar()
-    
-
-  except:
-    print('bucle terminado recargando pagina')
+    except Exception as e:
+        print(f"Ocurrió un error: {e}")
+    finally:
+        print("Cerrando navegador y esperando 5 minutos...")
+        driver.quit()
+        time.sleep(300)
